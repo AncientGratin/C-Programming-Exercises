9b4e043dbdbf595bf6046bd4c72d37b5af6e18c2
DongWook Lee <DongWook Lee>
2016-02-09 16:01:38 +0900

    add exercise0103.c

---
+++ exercise0103.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+main()
+{
+ float fahr, celsius;
+ int lower = 0, upper = 300, step = 20;
+ fahr = lower;
+
+  printf("============\n");
+
+  while(fahr <= upper)
+ {
+  celsius = (5.0 / 9.0) * (fahr - 32.0);
+  printf(" %3.0f %6.1f\n", fahr, celsius);
+  fahr += step;
+ }
+
+  printf("============\n");
+}
