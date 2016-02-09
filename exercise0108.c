26959b69646e5443b5bea3bae4af478b47df172d
DongWook Lee <DongWook Lee>
2016-02-09 16:19:01 +0900

    add exercise0107.c exercise0108.c

---
+++ exercise0108.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+main()
+{
+ int c, nb = 0, nt = 0, nl = 0;
+ while((c = getchar()) != EOF)
+ {
+  switch(c)
+  {
+  case ' ':
+   nb++;
+   break;
+  case '\t':
+   nt++;
+   break;
+  case '\n':
+   nl++;
+   break;
+  }
+ }
+ printf("count of\n");
+ printf(" blanks   = %d\n", nb);
+ printf(" tabs     = %d\n", nt);
+ printf(" newlines = %d\n", nl);
+}
